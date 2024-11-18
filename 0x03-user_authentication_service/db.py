2b648edab4d5de63dbd6ba18da3e7b5e98d8e38c
ETHreds <mjoejnr@gmail.com>
2024-11-19 00:40:04 +0300

    update user details

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,12 +3,14 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
-from user import Base, User
 
+from user import Base, User
 
 class DB:
     """DB class
@@ -43,3 +45,35 @@ class DB:
         self._session.commit()
 
         return new_user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering.
+
+        Returns:
+            User: The first matching user.
+
+        Raises:
+            NoResultFound: If no user matches the filter.
+            InvalidRequestError: If the query arguments are invalid.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the given parameters.")
+        except InvalidRequestError:
+            raise InvalidRequestError("Unsupported operation.")
+
+    def update_user(self, user_id: int, **updates) -> None:
+        user = self.find_user_by(id=user_id)
+
+        for key,value in updates.items():
+            if not hasattr(user, key):
+                raise ValueError(f"Invalid attribute '{key}' for user.")
+
+            setattr(user, key, value)
+        
+        self.__session.commit()
